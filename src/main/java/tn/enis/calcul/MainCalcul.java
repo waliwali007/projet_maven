11766471a4b2fbf56743f032f9e0e04de987c411
Ahmed Wali <ahmwali10@gmail.com>
2025-12-04 16:56:37 +0100

    ajouter classe main

---
+++ src/main/java/tn/enis/calcul/MainCalcul.java
@@ -0,0 +1,8 @@
+package tn.enis.calcul;
+
+public class MainCalcul {
+	public static void main(String[] args) {
+		CalculMetier cm = new CalculMetier();
+		System.out.println(cm.somme(5, 10));
+	}
+}
