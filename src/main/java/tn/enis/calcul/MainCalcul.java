3e00a508a541c881c41c96e7b137cb0cd09e7149
Ahmed Wali <147335195+waliwali007@users.noreply.github.com>
2025-12-04 23:26:51 +0100

    webhook et jenkins apres le push

--- src/main/java/tn/enis/calcul/MainCalcul.java
+++ src/main/java/tn/enis/calcul/MainCalcul.java
@@ -4,6 +4,6 @@ public class MainCalcul {
 	public static void main(String[] args) {
 		CalculMetier cm = new CalculMetier();
 		System.out.println(cm.somme(5, 10));
-		System.out.println(cm.produit(5,1));
+		System.out.println(cm.produit(5,10));
 	}
 }
